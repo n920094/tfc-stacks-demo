f5c597e3e60c0d872f2cff91524e5c6bdbdba360
Minoru Nakabayashi <nakabayashi.m@teldevice.co.jp>
2024-11-29 16:20:20 +0900

    change db

--- database/main.tf
+++ database/main.tf
@@ -11,16 +11,19 @@ module "mysql" {
   source  = "Azure/avm-res-dbformysql-flexibleserver/azurerm"
   version = "0.1.0"
 
-  name                      = local.name
-  location                  = azurerm_resource_group.main.location
-  resource_group_name       = azurerm_resource_group.main.name
-  administrator_login       = "mradministrator"
-  administrator_password    = "P@ssw0rd12345!"
-  sku_name                  = "GP_Standard_D2ds_v4"
-  mysql_version             = "8.0.21"
-  zone                      = "1"
-  standby_availability_zone = "2"
-  tags                      = var.tags
+  name                   = local.name
+  location               = azurerm_resource_group.main.location
+  resource_group_name    = azurerm_resource_group.main.name
+  administrator_login    = "mradministrator"
+  administrator_password = "P@ssw0rd12345!"
+  sku_name               = "GP_Standard_D2ds_v4"
+  mysql_version          = "8.0.21"
+  zone                   = 1
+  high_availability = {
+    mode                      = "ZoneRedundant"
+    standby_availability_zone = 2
+  }
+  tags = var.tags
 
   /*
   name                   = local.name
