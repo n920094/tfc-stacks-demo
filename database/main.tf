c3b45cc837b49724ea0b9537ee3a1b747c297139
Minoru Nakabayashi <nakabayashi.m@teldevice.co.jp>
2024-11-29 15:34:34 +0900

    change db

--- database/main.tf
+++ database/main.tf
@@ -8,14 +8,18 @@ resource "azurerm_resource_group" "main" {
 }
 
 module "avm-res-dbforpostgresql-flexibleserversql_server" {
-  source = "Azure/avm-res-dbforpostgresql-flexibleserver/azurerm"
+  source  = "Azure/avm-res-dbforpostgresql-flexibleserver/azurerm"
   version = "0.1.2"
 
   name                   = local.name
   location               = azurerm_resource_group.main.location
   resource_group_name    = azurerm_resource_group.main.name
+  administrator_password = "P@ssw0rd12345!"
+  sku_name               = "GP_Standard_D2ds_v4"
+  tags                   = var.tags
 
- /*
+
+  /*
   name                   = local.name
   location               = azurerm_resource_group.main.location
   resource_group_name    = azurerm_resource_group.main.name
