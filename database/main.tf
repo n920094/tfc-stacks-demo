656165d831e860e5bacbe6020e17a4c84a1d4510
Minoru Nakabayashi <nakabayashi.m@teldevice.co.jp>
2024-11-29 15:10:46 +0900

    change db

--- database/main.tf
+++ database/main.tf
@@ -19,7 +19,7 @@ module "avm-res-dbforpostgresql-flexibleserversql_server" {
   administrator_login    = "mradministrator"
   administrator_password = "P@ssw0rd12345!"
   sku_name               = "GP_Standard_D2ds_v4"
-  create_mode            = "Default"
-
+  create_mode            = "DefPointInTimeRestoreault"
+  source_server_id       = module.avm-res-dbforpostgresql-flexibleserversql_server.pg_id
   tags = var.tags
 }
