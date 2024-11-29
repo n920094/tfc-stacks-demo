3fcc7f7d96dc616c36ac53defa4014aebb362aac
Minoru Nakabayashi <nakabayashi.m@teldevice.co.jp>
2024-11-29 15:12:07 +0900

    change db

--- database/main.tf
+++ database/main.tf
@@ -8,7 +8,7 @@ resource "azurerm_resource_group" "main" {
 }
 
 module "avm-res-dbforpostgresql-flexibleserversql_server" {
-  source = "Azure/avm-res-dbforpostgresql-flexibleserver/azurerm"
+  source  = "Azure/avm-res-dbforpostgresql-flexibleserver/azurerm"
   version = "0.1.2"
   #source  = "app.terraform.io/TED_EVAL/avm-res-dbforpostgresql-flexibleserver/azurerm"
   #version = "1.0.0"
@@ -21,5 +21,5 @@ module "avm-res-dbforpostgresql-flexibleserversql_server" {
   sku_name               = "GP_Standard_D2ds_v4"
   create_mode            = "DefPointInTimeRestoreault"
   source_server_id       = module.avm-res-dbforpostgresql-flexibleserversql_server.pg_id
-  tags = var.tags
+  tags                   = var.tags
 }
